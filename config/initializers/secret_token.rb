843ec50bb8003ab6dc357140a8accb918b85a7a3
Charles Sublette <charlessublette@Charless-MacBook-Pro.local>
2014-02-20 14:30:27 -0700

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pmtool::Application.config.secret_key_base = 'e7115ad5739d976893b9fac6755719c2f0b1ebfeb09e5484e63625da7ab1c57d18f54a272ed0d2312b525496e29428ee0c87396e5379deeb6d3194056c3d0ee8'
